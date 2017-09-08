96b6032b4491c585c51e9b4b01bbbaba2b5d80a0
Fernando del olmo martin <fer.olmo92@gmail.com>
2017-09-08 08:32:34 +0200

    feat(): Added timeout to abort request

--- src/components/st-payment/st-payment.tsx
+++ src/components/st-payment/st-payment.tsx
@@ -9,9 +9,11 @@ export class StPayment {
 
   @Prop() methodData: any; // required payment method data
   @Prop() details: any;    // required information about transaction
-  @Prop() options: any;    // optional parameter for things like shipping, etc.
+  @Prop() options: any;    // optional parameter for things like shipping, etc
+  @Prop() timeout: any;    // timeout in minutes to abort the request
 
   doPayment() {
+    // Create Payment request
     let request = new PaymentRequest(
       this.methodData,
       this.details,
@@ -19,16 +21,35 @@ export class StPayment {
     );
 
     request.show().then(function(paymentResponse) {
+      let paymentData = {
+        // payment method string, e.g. “visa”
+        method: paymentResponse.methodName,
+        // payment details as you requested
+        details: paymentResponse.details
+      };
+      console.log('Payment success', paymentData);
       paymentResponse.complete("success");
     }).catch(function(err) {
       console.error("Uh oh, something bad happened", err.message);
     });
+
+    // Add some timeout to abort request
+    let paymentTimeout = window.setTimeout(function() {
+      window.clearTimeout(paymentTimeout);
+      request.abort().then(function() {
+        console.log(`Payment timed out after ${this.timeout} minutes.`);
+      }).catch(function() {
+        console.log('Unable to abort.');
+      });
+    }, this.timeout * 60 * 1000);
   }
 
   render() {
     return (
       <div>
-        <button>Buy</button>
+        <button onClick={() => this.doPayment()}>
+          Buy
+        </button>
       </div>
     );
   }
