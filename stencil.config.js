57f7a5dc9c5a4ca54d5460fc4a4b67c3fd0db557
Fernando del olmo <fer.olmo92@gmail.com>
2017-09-20 19:06:03 +0200

    feat(): Add publish and build flow

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,9 @@
 exports.config = {
+  namespace: 'stpayment',
+  generateDistribution: true,
+  generateWWW: false,
   bundles: [
-    { components: ['st-payment', 'demo-payment'] }
+    { components: ['st-payment'] }
   ]
 };
 
